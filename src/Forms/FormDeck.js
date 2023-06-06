b76249cb1479c2f22d8964c95700dc1603ae95f8
Chris <chrisaaron184@gmail.com>
2023-06-05 17:32:18 -0700

    BreadCrumb Path FormCard & FormDeck

--- src/Forms/FormDeck.js
+++ src/Forms/FormDeck.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function FormDeck() {
     const { deckId } = useParams();
 
-    const placeHolder = 
-        <p>
-            Edit Deck with id {deckId}
-        </p>;
+    const placeHolder = <p>Edit Deck with id {deckId}</p>;
     
-    return placeHolder;
+    return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <Link to={`/decks/${deckId}`}>Name of Deck {deckId}</Link>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                        Edit Page
+                    </li>
+                </ol>
+            </nav>
+            {placeHolder}
+        </div>
+    );
 }
 
 export default FormDeck;
